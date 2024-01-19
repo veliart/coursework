4b2b87657a936888f1574f937c86297777237b14
Artur Vakhitov <arthurvakhitov@gmail.com>
2024-01-19 15:03:34 +0300

    add test for JavaQuestionService

--- src/test/java/com/example/coursework/service/impl/JavaQuestionServiceImplTest.java
+++ src/test/java/com/example/coursework/service/impl/JavaQuestionServiceImplTest.java
@@ -1,13 +1,11 @@
 package com.example.coursework.service.impl;
 
 import com.example.coursework.model.Question;
-import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
 public class JavaQuestionServiceImplTest {
     private final JavaQuestionServiceImpl javaQuestionService = new JavaQuestionServiceImpl() {{
@@ -47,7 +45,22 @@ public class JavaQuestionServiceImplTest {
         }};
         //when
         Collection<Question> actualQuestions = javaQuestionService.getAll();
+        // then
         Assertions.assertEquals(expectedQuestions, actualQuestions);
+    }
+    @Test
+    public void shouldGetRandomQuestionIsCorrect() {
+        Collection<Question> expectedQuestions = new HashSet<>() {{
+            add(new Question("Question1", "Answer1"));
+            add(new Question("Question2", "Answer2"));
+            add(new Question("Question3", "Answer3"));
+            add(new Question("Question4", "Answer4"));
+            add(new Question("Question5", "Answer5"));
+        }};
+        //when
+        Question actualQuestion = javaQuestionService.getRandomQuestion();
+        // then
+        Assertions.assertTrue(expectedQuestions.contains(actualQuestion));
     }
 
 }
